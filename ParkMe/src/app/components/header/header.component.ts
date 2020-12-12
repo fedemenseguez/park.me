32add2951b629093011506de0e3c9a469f2cea67
Stefano Populin <tefapopulin@gmail.com>
2020-12-12 20:50:17 -0300

    Show logged user name

--- ParkMe/src/app/components/header/header.component.ts
+++ ParkMe/src/app/components/header/header.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
+  constructor(readonly router: Router, public authService: AuthService) { }
 
   reportFreeSpace() {
     this.router.navigate(['./free-space-report']);
   }
   goHome(){
     this.router.navigate(['./home']);
+  }
+
+  logout() {
+    this.authService.name = '';
+    sessionStorage.clear();
   }
 }
--- ParkMe/src/app/components/login/login.component.ts
+++ ParkMe/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +9,19 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(readonly route: ActivatedRoute, readonly router: Router) {}
+  constructor(
+    readonly route: ActivatedRoute,
+    readonly router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.route.fragment.subscribe((token: string) => {
       const rawToken = token;
       const parsedToken = rawToken.split('=')[1];
-      localStorage.setItem('token', parsedToken);
+      sessionStorage.setItem('token', parsedToken);
+      const decodedToken: any = jwt_decode(parsedToken);
+      this.authService.name = decodedToken.name;
       this.router.navigateByUrl('home');
     });
   }
