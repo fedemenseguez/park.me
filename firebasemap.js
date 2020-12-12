7637c89f90ad0d45b9acb0e787251bcd5b401042
Leonardo Gonzalez <leonardo.gonzalez@filtmtrack.net>
2020-12-12 15:56:31 -0300

    add slots

--- firebasemap.js
+++ firebasemap.js
@@ -1,5 +1,6 @@
 //AIzaSyCc-JEvXUfrQzDZaJ1ZZHQ8_7j02HazCtM
-
+let currentPosition = null;
+let slots = {};
 const config = {
     apiKey: "AIzaSyAuJ8u4qfLZIursyt0_-auisQvFezm1fi0",
     authDomain: "parkme-445d3.firebaseapp.com",
@@ -14,9 +15,18 @@ const config = {
 firebase.initializeApp(config);
 
 /**
+ * Icons.
+ */
+const icons = {
+    parking: "http://maps.google.com/mapfiles/kml/shapes/cross-hairs_highlight.png",
+    car: "http://earth.google.com/images/kml-icons/track-directional/track-0.png"
+};
+
+/**
  * Data object to be written to Firebase.
  */
 var data = { sender: null, timestamp: null, lat: null, lng: null };
+
 
 function makeInfoBox(controlDiv, map) {
     // Set CSS for the control border.
@@ -41,6 +51,42 @@ function makeInfoBox(controlDiv, map) {
     controlUI.appendChild(controlText);
 }
 
+
+function getTimestamp(addClick) {
+    // Reference to location for saving the last click time.
+    var ref = firebase.database().ref('last_message/' + data.sender);
+
+    ref.onDisconnect().remove();  // Delete reference from firebase on disconnect.
+
+    // Set value to timestamp.
+    ref.set(firebase.database.ServerValue.TIMESTAMP, function(err) {
+      if (err) {  // Write to last message was unsuccessful.
+        console.log(err);
+      } else {  // Write to last message was successful.
+        ref.once('value', function(snap) {
+          addClick(snap.val());  // Add click with same timestamp.
+        }, function(err) {
+          console.warn(err);
+        });
+      }
+    });
+  }
+
+
+function addToFirebase(data) {
+    debugger;
+    getTimestamp(function(timestamp) {
+      // Add the new timestamp to the record data.
+      data.timestamp = timestamp;
+      var ref = firebase.database().ref('slots').push(data, function(err) {
+        if (err) {  // Data was not written to firebase.
+          console.warn(err);
+        }
+      });
+    });
+  }
+
+
 /**
  * Creates a map object with a click listener and a heatmap.
  */
@@ -61,8 +107,9 @@ function initMap() {
         streetViewControl: false,
     });
 
+
     // Create the DIV to hold the control and call the makeInfoBox() constructor passing in this DIV.
-    let infoWindow = new google.maps.InfoWindow();
+    // let infoWindow = new google.maps.InfoWindow();
     var infoBoxDiv = document.createElement('div');
     makeInfoBox(infoBoxDiv, map);
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(infoBoxDiv);
@@ -70,18 +117,44 @@ function initMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                const pos = {
+                currentPosition = {
                     lat: position.coords.latitude,
                     lng: position.coords.longitude,
                 };
-                infoWindow.setPosition(pos);
-                infoWindow.setContent("Location found.");
-                infoWindow.open(map);
-                map.setCenter(pos);
+                // infoWindow.setPosition(currentPosition);
+                // infoWindow.setContent("Location found.");
+                // infoWindow.open(map);
+                map.setCenter(currentPosition);
+
+                const marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                    icon: icons.car,
+                    map: map,
+                });
             },
             () => {
                 handleLocationError(true, infoWindows, map.getCenter());
             }
         );
     }
+
+    var slots = firebase.database().ref('slots');
+    slots.once('value').then((snapshot) => {
+        snapshot.forEach(function(data) {
+            const marker = new google.maps.Marker({
+                position: new google.maps.LatLng(data.val().lat, data.val().lng),
+                icon: icons.parking,
+                map: map,
+            });
+            slots[data.key] = marker;
+        });
+    });
+
+    // Listen for clicks and add the location of the click to firebase.
+    map.addListener('click', function(e) {
+        const data = {};
+        data.lat = e.latLng.lat();
+        data.lng = e.latLng.lng();
+        addToFirebase(data);
+    });
 }
