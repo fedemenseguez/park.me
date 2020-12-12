66c26e02c406c1961bedb9b0fd761731e452465e
Federico Menseguez <federico.menseguez@filmtrack.net>
2020-12-12 15:14:47 -0300

    added maps and firebase

--- ParkMe/src/app/app.component.ts
+++ ParkMe/src/app/app.component.ts
@@ -1,5 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import firebase from 'firebase';
 
+const config = {
+  apiKey: "AIzaSyAuJ8u4qfLZIursyt0_-auisQvFezm1fi0",
+  authDomain: "parkme-445d3.firebaseapp.com",
+  databaseURL: 'https://parkme-445d3-default-rtdb.firebaseio.com/',
+  projectId: "parkme-445d3",
+  storageBucket: "parkme-445d3.appspot.com",
+  messagingSenderId: "960866532925",
+  appId: "1:960866532925:web:1f099b456759cf51963454",
+  measurementId: "G-3XEFMBDFKR"
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +18,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ParkMe';
+
+  ngOnInit(): void {    
+    firebase.initializeApp(config);
+  }
 }
